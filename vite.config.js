cce556af80d39ed1d3f1c3a21cba470a1b4c8832
kapil <kapil.personal9711@gmail.com>
2024-04-16 11:18:00 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./monster-solodex",
+  base: "/monster-solodex/.",
   plugins: [react()],
 });
